bfe83382727651dc556e0f628ce2b5030e97622f
Alexander Iglin <savinggalaxyforfood@gmail.com>
2019-01-04 01:11:15 +0400

    binding spells and item by link

--- main.lua
+++ main.lua
@@ -19,12 +19,12 @@ end
 SLASH_CTHUN1 = "/cthun"
 
 function ProcessInput(msg)
-    ChatFrame1:AddMessage("You've entered: ".. msg)
+    DEFAULT_CHAT_FRAME:AddMessage("You've entered: ".. msg)
     msg = trim(msg)
     local words = {}
     for word in msg:gmatch("%w+") do
 --    for word in msg:gmatch("%S+") do
-        ChatFrame1:AddMessage("Word: ".. word)
+        DEFAULT_CHAT_FRAME:AddMessage("Word: ".. word)
         table.insert(words, word)
     end
 --    local action_type = strupper(words[1])
@@ -34,7 +34,7 @@ function ProcessInput(msg)
     elseif (words[2] == "Hitem") then
         action_type = "ITEM"
     else
-        ChatFrame1:AddMessage("Incorrect command: you can only bind key to a spell or item!".. word)
+        DEFAULT_CHAT_FRAME:AddMessage("Incorrect command: you can only bind key to a spell or item!".. word)
         return 1
     end
 --    ChatFrame1:AddMessage("action_type=".. action_type)
@@ -53,30 +53,32 @@ end
 local EventFrame = CreateFrame("Frame")
 EventFrame:RegisterEvent("PLAYER_LOGIN")
 EventFrame:SetScript("OnEvent", function(_, _, _)
-    ChatFrame1:AddMessage("Hi, ".. UnitName("Player").. "! You can type \"/cthun\" to start binding abilities and macros on keys.")
+    DEFAULT_CHAT_FRAME:AddMessage("Hi, ".. UnitName("Player").. "! You can type \"/cthun\" to start binding abilities and macros on keys.")
 end)
 
 function SaveBinding(action_type, spell_or_item_id, key)
-    ChatFrame1:AddMessage("action_type=".. action_type)
-    ChatFrame1:AddMessage("spell or item id=".. spell_or_item_id)
-    ChatFrame1:AddMessage("key=".. key)
+    DEFAULT_CHAT_FRAME:AddMessage("action_type=".. action_type)
+    DEFAULT_CHAT_FRAME:AddMessage("spell or item id=".. spell_or_item_id)
+    DEFAULT_CHAT_FRAME:AddMessage("key=".. key)
     local ok
     if (action_type == "SPELL") then
 --        local spellName, spellSubName = GetSpellBookItemName(spell_or_item_id, BOOKTYPE_SPELL)
-        local spellName, spellSubName = GetSpellBookItemName(spell_or_item_id, "spell")
-        ChatFrame1:AddMessage("spellName=".. spellName)
-        ok = SetBinding(key, action_type + " " + spellName);
+        local spellName, rank, icon, castTime, minRange, maxRange, spellId = GetSpellInfo(spell_or_item_id)
+        DEFAULT_CHAT_FRAME:AddMessage("spellName=".. spellName)
+--        ok = SetBinding(key, action_type + " " + spellName);
+        ok = SetBindingSpell(key, spellName);
     else
         local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount,
         itemEquipLoc, itemIcon, itemSellPrice, itemClassID, itemSubClassID, bindType, expacID, itemSetID,
         isCraftingReagent = GetItemInfo(spell_or_item_id)
-        ChatFrame1:AddMessage("itemName=".. itemName)
-        ok = SetBinding(key, action_type + " " + itemName);
+        DEFAULT_CHAT_FRAME:AddMessage("itemName=".. itemName)
+--        ok = SetBinding(key, action_type + " " + itemName);
+        ok = SetBindingItem(key, itemName);
     end
     if (ok == 1) then
-        ChatFrame1:AddMessage("Binding failed, please, check your command!")
+        DEFAULT_CHAT_FRAME:AddMessage("Binding failed, please, check your command!")
     else
-        ChatFrame1:AddMessage("Binded sucessfully!")
+        DEFAULT_CHAT_FRAME:AddMessage("Binded sucessfully!")
     end
 end
 
